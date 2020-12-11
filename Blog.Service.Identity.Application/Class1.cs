26849cfa25104dc6775688c774124a7b642b3757
Mohammad  Abu Musa Rabiul <mohammad.rabiul@bordatech.com>
2020-12-11 23:00:17 +0300

    Inital commit

---
+++ Blog.Service.Identity.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Blog.Service.Identity.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Blog.Service.Identity.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Blog.Service.Identity.Domain
+{
+    public class Class1
+    {
+    }
+}
