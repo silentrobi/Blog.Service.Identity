a9a1252f66a90f15c0e97f8f5859dee490324866
Mohammad  Abu Musa Rabiul <mohammad.rabiul@bordatech.com>
2020-12-20 17:18:42 +0300

    removed unnecessary files

---
+++ Blog.Service.Identity.Api/Controllers/v1/IdentityUserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Service.Identity.Api.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityUserController : ControllerBase
+    {
+    }
+}
--- Blog.Service.Identity.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Blog.Service.Identity.Domain
-{
-    public class Class1
-    {
-    }
-}
